40db0341d643b737b17cea436e1a89d04e261ef6
menty44 <menty44@gmail.com>
2015-11-02 10:02:47 +0300

    first commit

---
+++ db_config.php
@@ -0,0 +1,11 @@
+<?php
+header('Content-Type: charset=utf-8');
+/*
+ * All database connection variables
+ */
+
+define('DB_USER', "root"); // db user
+define('DB_PASSWORD', "root"); // db password (mention your db password here)
+define('DB_DATABASE', "androidhive"); // database name
+define('DB_SERVER', "127.0.0.1"); // db server
+?>
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+header('Content-Type: charset=utf-8');
+    echo "Welcome, I am connecting Android to PHP, MySQL";
+?>
